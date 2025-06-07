840e69716825edfbfa674fa24ca87e6ae6e84388
zguy <zguy@zguy.dev>
2025-06-07 21:05:36 +0300

    refactor: OpenAPI example

--- examples/OpenAI/gen.go
+++ examples/OpenAI/gen.go
@@ -1,3 +1,3 @@
 package openapi_example
 
-//go:generate go run github.com/zguydev/openapi-fixer ./openapi.yaml fixed.openapi.yaml --fixups ./fixups/ --config openapi-fixer.yaml
+//go:generate go run github.com/zguydev/openapi-fixer openapi.yaml fixed.openapi.yaml --fixups ./fixups/ --config .openapi-fixer.yaml
